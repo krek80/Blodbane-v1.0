ad28792b168c6a50b1a623610a1dcc1ddb723876
krek80 <kristofferstrom@hotmail.com>
2017-03-21 22:21:09 +0100

    Registreringsskjema

--- Blodbane v1.0/blodbane.Designer.vb
+++ Blodbane v1.0/blodbane.Designer.vb
@@ -25,32 +25,40 @@ Partial Class Blodbane
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Blodbane))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FilToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.LoggPåToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.LoggAvToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AvsluttToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.InstillingerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox6 = New System.Windows.Forms.TextBox()
-        Me.TextBox7 = New System.Windows.Forms.TextBox()
-        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.LoggPåansattToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
         Me.MenuStrip1.SuspendLayout()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.TableLayoutPanel2.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -61,14 +69,9 @@ Partial Class Blodbane
         '
         'FilToolStripMenuItem
         '
-        Me.FilToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoggPåToolStripMenuItem, Me.LoggAvToolStripMenuItem, Me.AvsluttToolStripMenuItem})
+        Me.FilToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoggPåansattToolStripMenuItem, Me.LoggAvToolStripMenuItem, Me.AvsluttToolStripMenuItem})
         Me.FilToolStripMenuItem.Name = "FilToolStripMenuItem"
         resources.ApplyResources(Me.FilToolStripMenuItem, "FilToolStripMenuItem")
-        '
-        'LoggPåToolStripMenuItem
-        '
-        Me.LoggPåToolStripMenuItem.Name = "LoggPåToolStripMenuItem"
-        resources.ApplyResources(Me.LoggPåToolStripMenuItem, "LoggPåToolStripMenuItem")
         '
         'LoggAvToolStripMenuItem
         '
@@ -99,108 +102,173 @@ Partial Class Blodbane
         'TableLayoutPanel2
         '
         resources.ApplyResources(Me.TableLayoutPanel2, "TableLayoutPanel2")
-        Me.TableLayoutPanel2.Controls.Add(Me.Label9, 0, 7)
-        Me.TableLayoutPanel2.Controls.Add(Me.TextBox7, 1, 6)
-        Me.TableLayoutPanel2.Controls.Add(Me.TextBox6, 1, 5)
-        Me.TableLayoutPanel2.Controls.Add(Me.TextBox5, 1, 4)
-        Me.TableLayoutPanel2.Controls.Add(Me.TextBox4, 1, 3)
-        Me.TableLayoutPanel2.Controls.Add(Me.TextBox3, 1, 2)
-        Me.TableLayoutPanel2.Controls.Add(Me.Label4, 0, 2)
-        Me.TableLayoutPanel2.Controls.Add(Me.Label3, 0, 1)
-        Me.TableLayoutPanel2.Controls.Add(Me.TextBox1, 1, 0)
-        Me.TableLayoutPanel2.Controls.Add(Me.TextBox2, 1, 1)
-        Me.TableLayoutPanel2.Controls.Add(Me.Label2, 0, 0)
-        Me.TableLayoutPanel2.Controls.Add(Me.Label5, 0, 3)
-        Me.TableLayoutPanel2.Controls.Add(Me.Label6, 0, 4)
-        Me.TableLayoutPanel2.Controls.Add(Me.Label7, 0, 5)
-        Me.TableLayoutPanel2.Controls.Add(Me.Label8, 0, 6)
-        Me.TableLayoutPanel2.Controls.Add(Me.TextBox8, 1, 7)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBox7, 1, 7)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label8, 0, 7)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBox6, 1, 6)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label7, 0, 6)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label6, 0, 5)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBox4, 1, 4)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label5, 0, 4)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label4, 0, 3)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBox3, 1, 3)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBox2, 1, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label3, 0, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label2, 0, 1)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label9, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBox5, 1, 5)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBox1, 2, 1)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBox8, 3, 5)
         Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
         '
-        'TextBox1
+        'TextBox7
         '
-        resources.ApplyResources(Me.TextBox1, "TextBox1")
-        Me.TextBox1.Name = "TextBox1"
+        resources.ApplyResources(Me.TextBox7, "TextBox7")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.TextBox7, 2)
+        Me.TextBox7.Name = "TextBox7"
         '
-        'TextBox2
+        'TextBox6
         '
-        resources.ApplyResources(Me.TextBox2, "TextBox2")
-        Me.TextBox2.Name = "TextBox2"
-        '
-        'Label2
-        '
-        resources.ApplyResources(Me.Label2, "Label2")
-        Me.Label2.Name = "Label2"
-        '
-        'Label3
-        '
-        resources.ApplyResources(Me.Label3, "Label3")
-        Me.Label3.Name = "Label3"
-        '
-        'Label4
-        '
-        resources.ApplyResources(Me.Label4, "Label4")
-        Me.Label4.Name = "Label4"
-        '
-        'Label5
-        '
-        resources.ApplyResources(Me.Label5, "Label5")
-        Me.Label5.Name = "Label5"
-        '
-        'Label6
-        '
-        resources.ApplyResources(Me.Label6, "Label6")
-        Me.Label6.Name = "Label6"
-        '
-        'Label7
-        '
-        resources.ApplyResources(Me.Label7, "Label7")
-        Me.Label7.Name = "Label7"
-        '
-        'Label8
-        '
-        resources.ApplyResources(Me.Label8, "Label8")
-        Me.Label8.Name = "Label8"
-        '
-        'TextBox3
-        '
-        resources.ApplyResources(Me.TextBox3, "TextBox3")
-        Me.TextBox3.Name = "TextBox3"
-        '
-        'TextBox4
-        '
-        resources.ApplyResources(Me.TextBox4, "TextBox4")
-        Me.TextBox4.Name = "TextBox4"
+        resources.ApplyResources(Me.TextBox6, "TextBox6")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.TextBox6, 2)
+        Me.TextBox6.Name = "TextBox6"
         '
         'TextBox5
         '
         resources.ApplyResources(Me.TextBox5, "TextBox5")
         Me.TextBox5.Name = "TextBox5"
         '
-        'TextBox6
+        'TextBox4
         '
-        resources.ApplyResources(Me.TextBox6, "TextBox6")
-        Me.TextBox6.Name = "TextBox6"
+        resources.ApplyResources(Me.TextBox4, "TextBox4")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.TextBox4, 2)
+        Me.TextBox4.Name = "TextBox4"
         '
-        'TextBox7
+        'TextBox3
         '
-        resources.ApplyResources(Me.TextBox7, "TextBox7")
-        Me.TextBox7.Name = "TextBox7"
+        resources.ApplyResources(Me.TextBox3, "TextBox3")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.TextBox3, 2)
+        Me.TextBox3.Name = "TextBox3"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Label4, 2)
+        Me.Label4.Name = "Label4"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Label3, 2)
+        Me.Label3.Name = "Label3"
+        '
+        'TextBox1
+        '
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.TextBox1, 2)
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'TextBox2
+        '
+        resources.ApplyResources(Me.TextBox2, "TextBox2")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.TextBox2, 2)
+        Me.TextBox2.Name = "TextBox2"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Label2, 2)
+        Me.Label2.Name = "Label2"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Label5, 2)
+        Me.Label5.Name = "Label5"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Label6, 2)
+        Me.Label6.Name = "Label6"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Label7, 2)
+        Me.Label7.Name = "Label7"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Label8, 2)
+        Me.Label8.Name = "Label8"
+        '
+        'LoggPåansattToolStripMenuItem
+        '
+        Me.LoggPåansattToolStripMenuItem.Name = "LoggPåansattToolStripMenuItem"
+        resources.ApplyResources(Me.LoggPåansattToolStripMenuItem, "LoggPåansattToolStripMenuItem")
+        '
+        'Label9
+        '
+        resources.ApplyResources(Me.Label9, "Label9")
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Label9, 4)
+        Me.Label9.Name = "Label9"
         '
         'TextBox8
         '
         resources.ApplyResources(Me.TextBox8, "TextBox8")
         Me.TextBox8.Name = "TextBox8"
         '
-        'Label9
+        'TableLayoutPanel3
         '
-        resources.ApplyResources(Me.Label9, "Label9")
-        Me.Label9.Name = "Label9"
+        resources.ApplyResources(Me.TableLayoutPanel3, "TableLayoutPanel3")
+        Me.TableLayoutPanel3.Controls.Add(Me.TextBox10, 1, 2)
+        Me.TableLayoutPanel3.Controls.Add(Me.Button1, 0, 3)
+        Me.TableLayoutPanel3.Controls.Add(Me.Label10, 0, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.Label11, 0, 1)
+        Me.TableLayoutPanel3.Controls.Add(Me.Label12, 0, 2)
+        Me.TableLayoutPanel3.Controls.Add(Me.TextBox9, 1, 1)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.TableLayoutPanel3.SetColumnSpan(Me.Button1, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        resources.ApplyResources(Me.Label10, "Label10")
+        Me.TableLayoutPanel3.SetColumnSpan(Me.Label10, 2)
+        Me.Label10.Name = "Label10"
+        '
+        'Label11
+        '
+        resources.ApplyResources(Me.Label11, "Label11")
+        Me.Label11.Name = "Label11"
+        '
+        'Label12
+        '
+        resources.ApplyResources(Me.Label12, "Label12")
+        Me.Label12.Name = "Label12"
+        '
+        'TextBox9
+        '
+        resources.ApplyResources(Me.TextBox9, "TextBox9")
+        Me.TextBox9.Name = "TextBox9"
+        '
+        'TextBox10
+        '
+        resources.ApplyResources(Me.TextBox10, "TextBox10")
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.UseSystemPasswordChar = True
         '
         'Blodbane
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.TableLayoutPanel3)
         Me.Controls.Add(Me.TableLayoutPanel2)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Controls.Add(Me.MenuStrip1)
@@ -213,6 +281,8 @@ Partial Class Blodbane
         Me.TableLayoutPanel1.PerformLayout()
         Me.TableLayoutPanel2.ResumeLayout(False)
         Me.TableLayoutPanel2.PerformLayout()
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        Me.TableLayoutPanel3.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -223,11 +293,9 @@ Partial Class Blodbane
     Friend WithEvents LoggAvToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AvsluttToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents InstillingerToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents LoggPåToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents Label1 As Label
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
-    Friend WithEvents Label9 As Label
     Friend WithEvents TextBox7 As TextBox
     Friend WithEvents TextBox6 As TextBox
     Friend WithEvents TextBox5 As TextBox
@@ -242,5 +310,14 @@ Partial Class Blodbane
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
+    Friend WithEvents LoggPåansattToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label9 As Label
     Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TableLayoutPanel3 As TableLayoutPanel
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents TextBox9 As TextBox
 End Class
--- Blodbane v1.0/blodbane.vb
+++ Blodbane v1.0/blodbane.vb
@@ -5,11 +5,11 @@
 
     End Sub
 
-    Private Sub LoggPåToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoggPåToolStripMenuItem.Click
-        pålogging.Show()
-    End Sub
-
     Private Sub AvsluttToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AvsluttToolStripMenuItem.Click
         Me.Close()
+    End Sub
+
+    Private Sub LoggPåansattToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoggPåansattToolStripMenuItem.Click
+        pålogging.Show()
     End Sub
 End Class
